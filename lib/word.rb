4bcba4724d4698e2f28e9b85196acc9563eea672
Adam McFadden <mcfadden.113@gmail.com>
2015-05-01 09:41:11 -0700

    add methods to retrieve Word class array variable, save to that variable, and clear that variable. It will add corresponding specs and pass them.

--- lib/word.rb
+++ lib/word.rb
@@ -1,8 +1,22 @@
 class Word
-  
+
+  @@words = []
+
   attr_reader(:word)
 
   define_method(:initialize) do |attributes|
     @word = attributes.fetch(:word)
+  end
+
+  define_singleton_method(:all) do
+    @@words
+  end
+
+  define_method(:save) do
+    @@words.push(self)
+  end
+
+  define_singleton_method (:clear) do
+    @@words = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,11 +3,38 @@ require 'word'
 
 describe(Word) do
 
+  before do
+    Word.clear()
+  end
+
   describe('#word') do
     it('returns the word from a word object') do
       test_word = Word.new({:word => 'penguin'})
       expect(test_word.word()).to(eq('penguin'))
     end
   end
-  
+
+  describe('.all') do
+    it('will return all objects stored in the Word class array.Will be an empty array at initialize') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('save a Word object into a Word class array') do
+      test_word = Word.new({:word => 'elephant'})
+      test_word.save()
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe('.clear') do
+    it('will clear the Word class array') do
+      test_word = Word.new({:word => 'doggie'})
+      test_word.save()
+      Word.clear()
+      expect(Word.all()).to(eq([]))
+    end
+  end
+
 end
